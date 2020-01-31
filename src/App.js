16487451483d5148f67b5d902961857f2ae51bbc
eungalchi <tbvjzlem3@gmail.com>
2020-02-01 01:27:13 +0900

    first pelease

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const name = '리액트';
+  return(
+  <div>
+
+
+    {name === '리액트' ? (
+      <h2>
+        리액트입니다.
+      </h2>
+    ) : (
+      <h2>리액트가 아닙니다.</h2>
+    )}
+  </div>
+  );
+
+}
+
+export default App;
