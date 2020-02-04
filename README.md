1eb8c553bf003b5f18797b90d0219f6f2598bc22
eungalchi <tbvjzlem3@gmail.com>
2020-02-04 19:42:13 +0900

    ~4

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react practice for Naver burning Day Hackerton
+
+'리액트를 다루는 기술'
--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,7 +1,63 @@
+/* 함수형 컴포넌트 */
+
+import React, { useState } from 'react';
+
+const EventPractice = () => {
+  const [form, setForm] = useState({
+    username: '',
+    message: ''
+  });
+  const { username, message } = form;
+  const onChange = e => {
+    const nextForm = {
+      ...form, //기존의 form 내용을 이 자리에 복사!
+      [e.target.name]: e.target.value
+    };
+    setForm(nextForm);
+  };
+  const onClick = () => {
+    alert(username + ':' + message);
+    setForm({
+      username: '',
+      message: ''
+    });
+  };
+  const onKeyPress = e => {
+    if (e.key === 'Enter') {
+      onClick();
+    }
+  };
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        vlaue={username}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="입력하시오"
+        vlaue={message}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>확인버튼</button>
+    </div>
+  );
+};
+
+export default EventPractice;
+
+/* 클래스 컴포넌트
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
   state = {
+    username: '',
     message: ''
   };
 
@@ -16,7 +72,7 @@ class EventPractice extends Component {
 
   handleChange = e => {
     this.setState({
-      message: e.target.value
+      [e.target.name]: e.target.value //key를 []로 감싸면 그 안에 넣은 레퍼런스가 가리키는 실제 값이 key값으로 사용
     });
     //e객체는 SyntheticEvent로 웹 브라우저의 네이티브 이벤트를 감싸는 객체
     console.log(e); //이벤트가 끝나면 초기화되어 정보 참조 불가
@@ -24,10 +80,17 @@ class EventPractice extends Component {
   };
 
   handleClick = e => {
-    alert(this.state.message);
+    alert(this.state.username + ': ' + this.state.message);
     this.setState({
+      username: '',
       message: ''
     });
+  };
+
+  handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      this.handleClick();
+    }
   };
   render() {
     return (
@@ -35,10 +98,18 @@ class EventPractice extends Component {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인하였음</button>
       </div>
@@ -47,3 +118,4 @@ class EventPractice extends Component {
 }
 
 export default EventPractice;
+*/
