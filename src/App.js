7231fe40ea70e23c1a54b6d1a4ef47dcca8b734c
eungalchi <tbvjzlem3@gmail.com>
2020-02-05 02:31:17 +0900

    Use map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import logo from './logo.svg';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 import React, { Component } from 'react';
 
@@ -7,10 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ScrollBox ref={ref => (this.ScrollBox = ref)} />
-        <button onClick={() => this.ScrollBox.scrollToBotton()}>
-          맨 밑으로
-        </button>
+        <IterationSample />
       </div>
     );
   }
